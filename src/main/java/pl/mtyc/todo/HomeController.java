44a85a66a188ad26622149009fefa1b487a9eaa1
Marcin Tyc <marcintyc.7@gmail.com>
2020-03-01 23:17:33 +0100

    dodanie plików

--- src/main/java/pl/mtyc/todo/HomeController.java
+++ src/main/java/pl/mtyc/todo/HomeController.java
@@ -3,6 +3,7 @@ package pl.mtyc.todo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -22,15 +23,21 @@ public class HomeController {
         List<Task> tasksDone = taskRepository.findDone();
         model.addAttribute("tasks", tasks);
         model.addAttribute("zrobione", tasksDone);
-
+        model.addAttribute("add", tasks);
         return "home";
     }
 
     @GetMapping("/done")
-    public String done(@RequestParam Long id,Model model) {
+    public String done(@RequestParam Long id, Model model) {
         Task task = taskRepository.findById(id);
         task.setDone(true);
         taskRepository.save(task);
         return "redirect:/";
     }
+
+//    @PostMapping("/add")
+//    public String addTask(Task task) {
+//        taskRepository.save(task);
+//        return "redirect:/";
+//    }
 }
